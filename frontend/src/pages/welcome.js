050e6386d9a5483a7058003af9524801a6a0356b
geesssveee <sg7401@nyu.edu>
2024-07-20 15:35:57 -0700

    updates

--- frontend/src/pages/welcome.js
+++ frontend/src/pages/welcome.js
@@ -3,13 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { Box, Typography, Button, Container, TextField } from '@mui/material';
 import './welcome.css';
 import backgroundImage from '../Images/background.jpg';
+import axios from 'axios';
 
 const WelcomePage = () => {
   const [url, setUrl] = useState('');
   const navigate = useNavigate();
+
   const handleSubmit = async () => {
     try {
-      await axios.post('http://localhost:3000/product', { url }); // Sending the URL to the backend
+      await axios.post('http://localhost:3000/give_url', { url });
       navigate('/chat');
     } catch (error) {
       console.error('Error sending product data:', error);
@@ -26,7 +28,6 @@ const WelcomePage = () => {
           Want to know the best product? Chat with us and get quick answers!
         </Typography>
         
-        {/* Placeholder Input Field and Submit Button */}
         <Box mt={4} textAlign="center">
           <TextField
             label="Enter URL"
@@ -45,16 +46,6 @@ const WelcomePage = () => {
             Submit
           </Button>
         </Box>
-        
-        {/* Uncomment and use the following section if needed */}
-        {/* <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 4 }}>
-          <Button variant="contained" color="primary" component={Link} to="/login" sx={{ mr: 2 }}>
-            Log In
-          </Button>
-          <Button variant="contained" color="primary" component={Link} to="/signup">
-            Sign Up
-          </Button>
-        </Box> */}
       </Container>
     </Box>
   );
